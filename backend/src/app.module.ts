a3c85504faa098c6061b09f42717f72ce9496aa7
BohdanaKh <dananvm@gmail.com>
2024-05-23 14:17:39 +0300

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,16 +5,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { CardModule } from './card/card.module';
-import { DatabaseModule } from './database/database.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(
+      'mongodb://User2024K:Gru545!fl1L51@db:27017/management-boards?authSource=admin',
+    ),
     BoardModule,
     CardModule,
-    DatabaseModule,
+    // DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -5,13 +5,16 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('DATABASE_URI'),
-      }),
-      inject: [ConfigService],
-    }),
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => ({
+    //     uri: configService.get<string>('DATABASE_URI'),
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    MongooseModule.forRoot(
+      'mongodb://User2024K:Gru545!fl1L51@db:27017/management-boards?authSource=admin',
+    ),
   ],
 })
 export class DatabaseModule {}
